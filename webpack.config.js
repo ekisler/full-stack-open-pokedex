c68793f627df6d2c2dee3c0afc0a852ad1bd134b
Emisael Kisler <kisler.fullstack@gmail.com>
2024-08-29 14:16:59 -0400

    Corriegiendo archivo webpack type module ok

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,13 @@
-// eslint-disable-next-line no-undef
+/* eslint-disable no-undef */
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-// eslint-disable-next-line no-undef
-const config = require("./config");
-
-// eslint-disable-next-line no-undef
 const path = require("path");
 
-// eslint-disable-next-line no-undef
 module.exports = {
   entry: "./src/index.jsx",
   output: {
-    // eslint-disable-next-line no-undef
-    path: path.resolve(__dirname, config.build.dist),
+    path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-    // eslint-disable-next-line no-undef
-    publicPath: config.build.dist,
+    publicPath: "/",
   },
   module: {
     rules: [
@@ -48,21 +41,14 @@ module.exports = {
   },
   resolve: {
     extensions: ["*", ".js", ".jsx"],
-    alias: {
-      // eslint-disable-next-line no-undef
-      "@": path.resolve(__dirname, "src"),
-    },
   },
   devServer: {
     historyApiFallback: true,
-    // eslint-disable-next-line no-undef
-    contentBase: path.join(__dirname, config.build.dist), // Add this line
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: "./public/index.html",
       filename: "./index.html",
-      inject: false,
     }),
   ],
 };
